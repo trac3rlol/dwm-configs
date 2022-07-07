ffe4b4059303c14c8d5dc27bf8c05d6dcdc1b5ba
tracerlol <77352572+trac3rlol@users.noreply.github.com>
2022-07-07 13:59:46 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,26 +5,23 @@ echo "Would you like to continue with the setup? (y/n)"
 read choice
 
 if [[ $choice = "y" ]]; then
+    cd $HOME
     echo "Starting Dwm setup..."
-    mkdir -p $HOME/.dwm
     cp -r dwm/ $HOME/.dwm
     cd $HOME/.dwm/ && sudo make clean install && cd ..
     echo "Dwm config setup completed"
     sleep 1s
     echo "Starting Dmenu setup..."
-    mkdir -p $HOME/.config/dmenu
     cp -r dmenu/ $HOME/.config/dmenu
     cd $HOME/.config/dmenu/ && sudo make clean install && cd ..
     echo "Dmenu config setup completed"
     sleep 1s
     echo "Starting Slock setup..."
-    mkdir -p $HOME/.config/slock
     cp -r slock/ $HOME/.config/slock
     cd $HOME/.config/slock/ && sudo make clean install && cd ..
     echo "Slock config setup completed"
     sleep 1s
     echo "Starting Slstatus setup..."
-    mkdir -p $HOME/.config/slstatus
     cp -r slstatus/ $HOME/.config/slstatus
     cd $HOME/.config/slstatus/ && sudo make clean install && cd ..
     echo "Slstatus config setup completed"
