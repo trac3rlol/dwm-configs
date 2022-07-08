0a782ac85b2c129692b13c84670339e7b6a4461e
tracerlol <77352572+trac3rlol@users.noreply.github.com>
2022-07-08 13:31:52 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "DWM SETUP"
 pacman -S nitrogen libxft picom ttf-font-awesome ttf-hack ttf-nerd-fonts-symbols terminus-font
 sleep 2s
 mkdir /usr/share/xsessions/
-mv dwm.desktop /usr/share/xsessions/
+mv dwm/dwm.desktop /usr/share/xsessions/
 mv dwm /usr/local/
 mv dmenu /usr/local/
 mv slock /usr/local/
