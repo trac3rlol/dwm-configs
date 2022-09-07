099548696a8f1b66916165e684284fe943b6fa3b
trac3rlol <noukosfxbusiness@gmail.com>
2022-09-07 13:31:19 +0300

    dwm-flexipatch

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -2,5 +2,8 @@
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 picom --experimental-backends &
 wmname LG3D
+dunst -conf $HOME/.config/dunst/dunstrc &
 slstatus &
 nitrogen --restore &
+xrandr --rate 60
+$HOME/scripts/tray.sh &
